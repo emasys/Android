210defc5211846444345247f3b28f80f33bd8b77
Emmanuel Ndukwe <emasysnd@gmail.com>
2017-06-25 21:54:16 +0100

    added a coordinatorLayout

--- TourGuideApp/app/src/main/java/com/example/android/tourguideapp/MainActivity.java
+++ TourGuideApp/app/src/main/java/com/example/android/tourguideapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.tourguideapp;
 
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +17,20 @@ public class MainActivity extends AppCompatActivity {
 
         final Toolbar toolbar = (Toolbar)findViewById(R.id.MyToolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
-
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
+        collapsingToolbarLayout.setTitle("This Is LAGOS");
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.MyCustomTabTextAppearance);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.TextExpAppearance);
+        Typeface face= Typeface.createFromAsset(getAssets(), "fonts/Fugaz.ttf");
+        Typeface face1= Typeface.createFromAsset(getAssets(), "fonts/PassionOne.ttf");
 
-        collapsingToolbarLayout.setTitle("My Toolbar");
+        collapsingToolbarLayout.setCollapsedTitleTypeface(face);
+        collapsingToolbarLayout.setExpandedTitleTypeface(face1);
+
+
+
+        Context context = this;
+        collapsingToolbarLayout.setContentScrimColor(ContextCompat.getColor(context, R.color.colorPrimary));
     }
 }
